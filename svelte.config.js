a5f6988f4bb886286489574aff0b6d048346ca52
Gian Ott <nichtgian@gmail.com>
2025-04-12 03:54:48 +0200

    Try fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from '@sveltejs/adapter-static';
-// import process from 'node:process';
+import process from 'node:process';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
@@ -9,7 +9,7 @@ const config = {
             fallback: '404.html',
         }),
         paths: {
-            // base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
+            base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
             relative: false,
         },
     },
