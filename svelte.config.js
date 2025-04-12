fc6a0a0cb5935e800b54204c778078e3fada5728
Gian Ott <nichtgian@gmail.com>
2025-04-12 03:39:33 +0200

    Fix files not found

--- svelte.config.js
+++ svelte.config.js
@@ -10,6 +10,7 @@ const config = {
         }),
         paths: {
             // base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
+            relative: false,
         },
     },
 };
