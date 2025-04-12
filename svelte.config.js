b71bd8e72078658ccfc71213f57f0a41bb4b2b68
Gian Ott <nichtgian@gmail.com>
2025-04-12 03:29:45 +0200

    Fix files not found

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
-import process from 'node:process';
-// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+// import process from 'node:process';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
-    // preprocess: vitePreprocess(),
+    preprocess: vitePreprocess(),
     kit: {
         adapter: adapter({
             fallback: '404.html',
         }),
         paths: {
-            base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
+            // base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
         },
     },
 };
